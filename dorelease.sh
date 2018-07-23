b5c7c680616f1b32a5fcad35fbe41e836b82eb0b
Oliver Butterfield <olly@opb.me.uk>
2018-07-23 21:00:29 +0100

    rest different release method

---
+++ dorelease.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+printenv
+[ "$(TAG)" ] || exit 0;
+
+github-release release \
+    --security-token $(GT) \
+    --user opb \
+    --repo secretly \
+    --tag $(VERSION) \
+    --name $(VERSION)
+
+github-release upload \
+    --security-token $(GT) \
+    --user opb \
+    --repo secretly \
+    --tag $(VERSION) \
+    --name secretly-$(VERSION)-darwin-amd64 \
+    --file dist/secretly-$(VERSION)-darwin-amd64
+
+github-release upload \
+    --security-token $(GT) \
+    --user opb \
+    --repo secretly \
+    --tag $(VERSION) \
+    --name secretly-$(VERSION)-linux-amd64 \
+    --file dist/secretly-$(VERSION)-linux-amd64
